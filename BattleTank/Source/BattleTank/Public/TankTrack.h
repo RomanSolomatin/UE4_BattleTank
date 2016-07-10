430f3caabde36f82fe112dd61d98abf3136c97b9
unknown <jaimuepe@gmail.com>
2016-07-10 21:22:22 +0200

    BT 51 Applying throttle force

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -13,4 +13,8 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly, Category = Input)
+	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank @ 1g acceleration
 };
