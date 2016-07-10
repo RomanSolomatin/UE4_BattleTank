4217b9e65dd5330f0af8fe27d067ddf1b6271210
unknown <jaimuepe@gmail.com>
2016-07-10 20:41:34 +0200

    BT50 Setup throttle

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -5,7 +5,7 @@
 #include "Components/StaticMeshComponent.h"
 #include "TankBarrel.generated.h"
 
-UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))//, HideCategories = ( "Collision" ))
+UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
